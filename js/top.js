0f7a0b89e38459a64764982e3e910b835065f37c
Rafael Teles <rafael_tsalves@hotmail.com>
2024-12-14 00:33:18 -0300

    criação da page

---
+++ js/top.js
@@ -0,0 +1,17 @@
+//selecionar btn
+const topButton = document.getElementById("topbutton");
+
+// mostrar o btn
+window.onscroll = function() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+
+};
+
+// função para rolar
+topButton.onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth'});
+};
